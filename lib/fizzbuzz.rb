74f38bc760b69f2a19641e8753cc56181737e120
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-24 12:26:54 +0100

    Remove blank between exception handler and other code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,5 @@
 def fizzbuzz(number)
   raise ArgumentError, "You cannot pass the #{number.class} #{number} to #fizzbuzz, please try again with an Integer." unless number.is_a? Integer
-
   to_return = ''
   to_return += 'Fizz' if number%3 == 0
   to_return += 'Buzz' if number%5 == 0
