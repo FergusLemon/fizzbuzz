7dd928da806669c31febea921103b53e77a72918
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-24 09:45:32 +0100

    Update specs to remove duplication

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,22 +3,14 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
 
-  it 'returns "Fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'Fizz'
-  end
   it 'returns "Fizz" when passed a number divisible by 3' do
     expect(fizzbuzz(6)).to eq 'Fizz'
-  end
-  it 'returns "Buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'Buzz'
   end
   it 'returns "Buzz" when passed a number divisble by 5' do
     expect(fizzbuzz(25)).to eq 'Buzz'
   end
-  it 'returns "FizzBuzz" when passed 15' do
+  it 'returns "FizzBuzz" when passed a number divisible by 3 & 5' do
     expect(fizzbuzz(15)).to eq 'FizzBuzz'
-  end
-  it 'returns "FizzBuzz" when passed 45' do
     expect(fizzbuzz(45)).to eq 'FizzBuzz'
   end
   it 'returns the number passed in when it is not divisible by 3, 5 or both numbers' do
