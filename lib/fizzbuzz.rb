69be13615b6376260e0a0c4ed305afbfcaa7c571
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-24 12:07:08 +0100

    Refactor code to clean up repetition and line count

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,14 +1,9 @@
 def fizzbuzz(number)
   raise ArgumentError, "You cannot pass the #{number.class} #{number} to #fizzbuzz, please try again with an Integer." unless number.is_a? Integer
-  if number == 0
-    number
-  elsif number%3 == 0 && number%5 == 0
-    'FizzBuzz'
-  elsif number%5 == 0
-    'Buzz'
-  elsif number%3 == 0
-    'Fizz'
-  else
-    number
-  end
+
+  to_return = ''
+  to_return += 'Fizz' if number%3 == 0
+  to_return += 'Buzz' if number%5 == 0
+  to_return = number if to_return.length == 0 || number == 0
+  to_return
 end
