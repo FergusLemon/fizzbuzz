7af06d3fd4bb1fb8d804131676fa35ebdec3537a
Fergus Lemon <fergus.lemon@gmail.com>
2018-10-24 11:07:42 +0100

    Include tests for exception handling where non-integers are passed in

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,19 +2,33 @@ require 'spec_helper'
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+  context 'when passed an integer' do
+    it 'returns "Fizz" if the integer is divisible by 3' do
+      expect(fizzbuzz(6)).to eq 'Fizz'
+    end
+    it 'returns "Buzz" if the integer is divisble by 5' do
+      expect(fizzbuzz(25)).to eq 'Buzz'
+    end
+    it 'returns "FizzBuzz" if the integer is divisible by 3 & 5' do
+      expect(fizzbuzz(15)).to eq 'FizzBuzz'
+      expect(fizzbuzz(45)).to eq 'FizzBuzz'
+    end
+    it 'returns the integer when it is not divisible by 3, 5 or both' do
+      expect(fizzbuzz(101)).to eq 101
+      expect(fizzbuzz(0)).to eq 0
+    end
+  end
 
-  it 'returns "Fizz" when passed a number divisible by 3' do
-    expect(fizzbuzz(6)).to eq 'Fizz'
-  end
-  it 'returns "Buzz" when passed a number divisble by 5' do
-    expect(fizzbuzz(25)).to eq 'Buzz'
-  end
-  it 'returns "FizzBuzz" when passed a number divisible by 3 & 5' do
-    expect(fizzbuzz(15)).to eq 'FizzBuzz'
-    expect(fizzbuzz(45)).to eq 'FizzBuzz'
-  end
-  it 'returns the number passed in when it is not divisible by 3, 5 or both numbers' do
-    expect(fizzbuzz(101)).to eq 101
-    expect(fizzbuzz(0)).to eq 0
+  context 'when a non-integer is passed' do
+    it 'raises an Argument Error' do
+      expect { fizzbuzz(1.5) }.to raise_error { |error|
+      expect(error).to be_a(ArgumentError) }
+    end
+    it 'tells the user if a Float is passed' do
+      expect { fizzbuzz(1.5) }.to raise_error.with_message(/You cannot pass the Float 1.5 to #fizzbuzz/)
+    end
+    it 'tells the user if an Array is passed' do
+      expect { fizzbuzz([1, 2, 3]) }.to raise_error.with_message(/You cannot pass the Array \[1, 2, 3\] to #fizzbuzz/)
+    end
   end
 end
